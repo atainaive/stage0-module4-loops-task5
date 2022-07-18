14f28b1097f0039acb07ddcfa5e25cddb64b3e93
atainaive <atai.omorov@stud.vgtu.lt>
2022-07-18 14:38:21 +0300

    commited

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-        for (int i = 1; i <= height; i++) {
+        for (int i = 1; i <= height / 2; i++) {
             for (int k = 1; k <= i - 1; k++) {
                 System.out.print(" ");
             }
@@ -11,7 +11,7 @@ public class Hourglass {
             }
             System.out.println();
         }
-        for (int i = height - 1; i >= 1; i--) {
+        for (int i = height / 2; i >= 1; i--) {
             for (int k = 1; k <= i - 1; k++) {
                 System.out.print(" ");
             }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task5;
 public class Triangle {
     public void printTriangle(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
-            for (int j = 0; j <= i; j++) {
+            for (int j = 1; j <= i; j++) {
                     System.out.print("8");
             }
             System.out.println();
